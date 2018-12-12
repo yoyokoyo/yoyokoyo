1542544f10b35bc179cb72b6c82b48ae0b506130
Youssef <37754072+yoyokoyo@users.noreply.github.com>
2018-12-12 14:05:25 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true
+do
+
+wget https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.gz
+
+gunzip -f nvdcve-1.0-recent.json.gz
+
+jq -r '.CVE_data_timestamp' nvdcve-1.0-recent.json > actual_time.txt
+
+jq -r '.CVE_Items[].cve.CVE_data_meta.ID' nvdcve-1.0-recent.json > lines_of_cve.txt
+
+jq -r '.CVE_Items[].publishedDate' nvdcve-1.0-recent.json > lines_of_dates.txt
+
+jq -r '.CVE_Items[].cve.description.description_data[].value' nvdcve-1.0-recent.json > lines_of_desc.txt
+
+
+sleep 1800
+done
